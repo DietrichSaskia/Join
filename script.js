e65d92463dc5a853874b4c2c16b288788bbc8f49
Alexander Hardtke <5yl3kz@googlemail.com>
2024-08-14 17:12:40 +0200

    Firebase Server

---
+++ script.js
@@ -0,0 +1,16 @@
+init()
+
+async function init() {
+    let i = await fetch("https://join-317-default-rtdb.europe-west1.firebasedatabase.app/" + ".json")
+    let pokemon = await i.json();
+    console.log(pokemon);
+
+}
+
+let users = [
+    {
+        'email': 'test123@gmx.net',
+        'password': 'password123',
+        'contacts': '',
+    }, 
+]
