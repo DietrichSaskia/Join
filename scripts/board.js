6c9e63ec8a877d9cb1233275ebb2a1cc63af71c9
Saskia Dietrich <saskiadietrich@gmx.net>
2024-08-15 17:36:54 +0200

    board first commit

---
+++ scripts/board.js
@@ -0,0 +1,68 @@
+let board = [
+  {
+    'category': 'User Story', 
+    'title': 'implement summary',
+    'description': 'Set up a summary for the user',
+    'subtasks': [],
+    'assignedTo': [
+      'Anton Mayer',
+      'Matteo Santos',
+    ],
+    'prio': '<img src="/assets/icons/prioMedium.png" alt="Priority Icon">',
+  },
+  {
+    'category': 'User Story',
+    'title': 'user Login set up',
+    'description': 'Create a login page for the user',
+    'subtasks': [],
+    'assignedTo': [
+      'Pedro Gonzales',
+      'Anja Schulz'
+    ],
+    'prio': '<img src="/assets/icons/prioHight.png" alt="Priority Icon">',
+  },
+  {
+    'category': 'User Story',
+    'title': 'the user can add contacts',
+    'description': 'new contacts can be added using a button',
+    'subtasks': [],
+    'assignedTo': [
+      'Benedikt Ziegler',
+      'David Eisenberg'
+    ],
+    'prio': '<img src="/assets/icons/prioMedium.png" alt="Priority Icon">',
+  },
+  {
+    'category': 'technical Task',
+    'title': 'CSS Architecture Planning',
+    'description': 'define css classes and structure them',
+    'subtasks': [],
+    'assignedTo': [
+      'Emmanuel Mauer',
+      'Eva Fischer'
+    ],
+    'prio': '<img src="/assets/icons/prioMedium.png" alt="Priority Icon">',
+  },
+  {
+    'category': 'technical Task',
+    'title': 'HTML template header and footer',
+    'description': 'the same template header and footer for every page',
+    'subtasks': [],
+    'assignedTo': [
+      'Xavi Hernandez',
+      'Lisa Müller'
+    ],
+    'prio': '<img src="/assets/icons/prioMedium.png" alt="Priority Icon">',
+  },
+  {
+    'category': 'technical Task',
+    'title': 'responsive layout for the website',
+    'description': 'The layout is intended to ensure that the website looks good and is user-friendly on desktops, tablets and smartphones.',
+    'subtasks': [],
+    'assignedTo': [
+      'Benedikt Ziegler',
+      'David Eisenberg'
+    ],
+    'prio': '<img src="/assets/icons/prioLow.png" alt="Priority Icon">',
+  },
+];
