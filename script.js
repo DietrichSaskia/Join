2454b49769b312c9af19b319f4e85842fed01160
Alexander Hardtke <5yl3kz@googlemail.com>
2024-08-15 12:30:26 +0200

    Header in JS

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 init()
 
 async function init() {
+    renderHeader();
     let i = await fetch("https://join-317-default-rtdb.europe-west1.firebasedatabase.app/" + ".json")
     let pokemon = await i.json();
     console.log(pokemon);
