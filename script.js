cd5521798ebd69d352c6fb91b5b4d7a4f0f8a411
Alexander Englert <aenglertymail23@gmail.com>
2024-09-01 16:03:21 +0200

    finish 16

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+/**
+ * The following functions are executed when the page is loaded.
+ * 
+ * @param {*} id 
+ */
 function init(id) {
     renderHeader(id);
     checkCurrentPage();
@@ -5,6 +10,10 @@ function init(id) {
 }
 
 
+/**
+ * This checks where the user is currently located and classes are added depending on this.
+ * 
+ */
 function checkCurrentPage() {
     let i = window.location.href;
     currentPageHTML = i.substring(i.lastIndexOf('/')).replace('/', "");
@@ -19,11 +28,18 @@ function checkCurrentPage() {
 }
 
 
+/**
+ * This function takes you back to the previous window.
+ */
 function goBack() {
     window.history.back();
 }
 
 
+/**
+ * In this function, the current user is loaded from the local storage and it is checked whether the user is null or not.
+ * In addition, the initials are loaded at the desired position in the header.
+ */
 async function checkUser() {
     let name;
     let Currentname = localStorage.getItem('CurrentUser');
@@ -38,6 +54,16 @@ async function checkUser() {
     `;   
 }
 
+
+/**
+ * This function passes the current CurrentName and splits the first letter of the first name and last name.
+ * The system then checks whether initials has the length 1; if this is the case, then the guest has logged in.
+ * Finally, the initials are returned.
+ * 
+ * @param {*} Currentname 
+ * @param {*} name 
+ * @returns 
+ */
 function checkUser2(Currentname, name) {
     name = JSON.parse(Currentname);
     let nameParts = name.split(' ');
@@ -48,6 +74,10 @@ function checkUser2(Currentname, name) {
     return initials;
 }
 
+
+/**
+ * Classes are added here.
+ */
 function checkUser3() {
     document.getElementById('burgerMenu').classList.add('dNone');
     document.getElementById('HelpSideOpen').classList.add('dNone');
@@ -55,7 +85,9 @@ function checkUser3() {
 }
 
 
-
+/**
+ * Here it is checked whether the element has the class dNone and, depending on this, a different function is executed.
+ */
 function toggleBurgerMenu() {
     if (document.getElementById('burgerMenu').classList.contains("dNone")) {
         showBurgerMenu();
@@ -66,18 +98,28 @@ function toggleBurgerMenu() {
 }
 
 
+/**
+ * The element is visualised here.
+ */
 function showBurgerMenu() {
     document.getElementById('burgerMenu').classList.remove('dNone');
     document.getElementById('headerOverlay').classList.add('showHeaderOverlay');
 }
 
 
+/**
+ * The element is made invisible here.
+ */
 function hideBurgerMenu() {
     document.getElementById('burgerMenu').classList.add('dNone');
     document.getElementById('headerOverlay').classList.remove('showHeaderOverlay');
 }
 
 
+/**
+ * If the function is clicked, the Current User is set to zero in the local storage.
+ * This means that the saved name is reset as if no user is logged in.
+ */
 function resetUser() {
     let UserAsText = JSON.stringify(null);
     localStorage.setItem('CurrentUser', UserAsText);
